3d899dc52ea12c5b021fd50e93ceddf17d363a0a
JuanH <jhelos@gmail.com>
2026-04-23 21:13:10 -0400

    Controladores Login, Registro y Logout, Open/Closed Principle

---
+++ app/Http/Controllers/API/V1/Auth/LoginController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\API\V1\Auth;
+
+use App\Contracts\API\Auth\AuthServiceInterface;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\API\V1\Auth\LoginRequest;
+use App\Http\Requests\API\V1\Auth\RegisterRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function __construct(private readonly AuthServiceInterface $authService)
+    {
+        parent::__construct();
+    }
+
+    public function  __invoke(LoginRequest $request):JsonResponse
+    {
+        return $this->authService->register($request->validated());
+    }
+}
---
+++ app/Http/Controllers/API/V1/Auth/LogoutController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\API\V1\Auth;
+
+use App\Contracts\API\Auth\AuthServiceInterface;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\API\V1\Auth\RegisterRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class LogoutController extends Controller
+{
+    public function __construct(private readonly AuthServiceInterface $authService)
+    {
+        parent::__construct();
+    }
+
+    public function  __invoke():JsonResponse
+    {
+        return $this->authService->logout();
+    }
+}
